abc8d6a6201dd54559fc6b798cd126219ee62ca3
Toan Nguyen <toan.nguyen@hitatek.io>
2022-03-19 20:56:12 +0700

    fix package file

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # online-code-editor
 
-- This project is built with TS
-- Monorepo is used by lerna
-- code bundle by ESBuild
+1. This project is built with TS
+2. Monorepo is used by lerna
+3. code bundle by ESBuild
+   1. Bundling code remote or locally
+      - Remove and extra request to API server, make code executes faster.
+      - We don't have to maintain an API server.
+      - Less complexity - no moving code back and forth.
--- packages/jbook/src/index.tsx
+++ packages/jbook/src/index.tsx
@@ -3,45 +3,45 @@ import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  const ref = useRef<any>();
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
+    const ref = useRef<any>();
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
 
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: '/esbuild.wasm',
-    });
-  };
-  useEffect(() => {
-    startService();
-  }, []);
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm',
+        });
+    };
+    useEffect(() => {
+        startService();
+    }, []);
 
-  const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
+    const onClick = async () => {
+        if (!ref.current) {
+            return;
+        }
 
-    const result = await ref.current.transform(input, {
-      loader: 'jsx',
-      target: 'es2015',
-    });
+        const result = await ref.current.transform(input, {
+            loader: 'jsx',
+            target: 'es2015',
+        });
 
-    setCode(result.code);
-  };
+        setCode(result.code);
+    };
 
-  return (
-    <div>
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <pre>{code}</pre>
-    </div>
-  );
+    return (
+        <div>
+            <textarea
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            />
+            <div>
+                <button onClick={onClick}>Submit</button>
+            </div>
+            <pre>{code}</pre>
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
