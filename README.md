3d24c61ed066fd123edf3690fadd224d0e7a1c58
Toan Nguyen <toan.nguyen@hitatek.io>
2022-03-20 00:48:59 +0700

    solve issue bundle dependencies on browser

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
-# online-code-editor
+# Online Code Editor
+A CLI to launch an interactive development environment for writing and document code.
 
+Users are able to build tech note with markdown syntax, also use code editor to install an build module on an online tool.
 1. This project is built with TS
 2. Monorepo is used by lerna
-3. code bundle by ESBuild
+3. Code bundle by [ESBuild](https://esbuild.github.io)
    1. Bundling code remote or locally
       - Remove and extra request to API server, make code executes faster.
       - We don't have to maintain an API server.
       - Less complexity - no moving code back and forth.
+      - **Issue**: when bundle, ESBuild tries to find dependencies on hard drive, but the code is built on browser, so no file system and ESBuild will throw an error.
+        - solution: 
+          - We need to build a plugin to intercept ESBuild and find the path should be for the dependencies.
+          - Use [unpkg](https://unpkg.com) to overcome CORS issue when we get the package from npm: unpkg will redirect to main file (index) of current version of dependencies.
--- packages/jbook/src/index.tsx
+++ packages/jbook/src/index.tsx
@@ -1,33 +1,48 @@
 import * as esbuild from 'esbuild-wasm';
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
-    const ref = useRef<any>();
     const [input, setInput] = useState('');
     const [code, setCode] = useState('');
+    const [bundler, setBundler] = useState<any>(null);
 
     const startService = async () => {
-        ref.current = await esbuild.startService({
+        const esBundler = await esbuild.startService({
             worker: true,
             wasmURL: '/esbuild.wasm',
         });
+        setBundler(esBundler);
     };
     useEffect(() => {
         startService();
     }, []);
 
     const onClick = async () => {
-        if (!ref.current) {
+        if (!bundler) {
             return;
         }
 
-        const result = await ref.current.transform(input, {
-            loader: 'jsx',
-            target: 'es2015',
+        // const result = await bundler.transform(input, {
+        //     loader: 'jsx',
+        //     target: 'es2015',
+        // });
+        //
+        // setCode(result.code);
+
+        /**
+         * Use this function and config to intercept ESBuild
+         * to find the path should be for the dependencies
+         */
+        const result = await bundler.build({
+            entryPoints: ['index.ts'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin()],
         });
 
-        setCode(result.code);
+        setCode(result.outputFiles[0].text);
     };
 
     return (
