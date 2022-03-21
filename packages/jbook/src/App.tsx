f32b678ac441a00b9a4f959f72d2ee05cd6b7238
Toan Nguyen <toan.nguyen@hitatek.io>
2022-03-21 21:34:03 +0700

    fix eslint and add test tiny pkg

---
+++ packages/jbook/src/App.tsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+
+const App = () => {
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
+    const [bundler, setBundler] = useState<any>(null);
+
+    const startService = async () => {
+        const esBuildBundler = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        });
+        setBundler(esBuildBundler);
+    };
+    useEffect(() => {
+        startService();
+    }, []);
+
+    const onClick = async () => {
+        if (!bundler) {
+            return;
+        }
+
+        // const result = await bundler.transform(input, {
+        //     loader: 'jsx',
+        //     target: 'es2015',
+        // });
+        //
+        // setCode(result.code);
+
+        /**
+         * Use this function and config to intercept ESBuild
+         * to find the path should be for the dependencies
+         */
+        const result = await bundler.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin()]
+        });
+
+        // console.log(result);
+
+        setCode(result.outputFiles[0].text);
+    };
+
+    return (
+        <div>
+            <textarea value={input} onChange={(e) => setInput(e.target.value)} />
+            <div>
+                <button onClick={onClick}>Submit</button>
+            </div>
+            <pre>{code}</pre>
+        </div>
+    );
+};
+
+export default App;
--- packages/jbook/src/index.tsx
+++ packages/jbook/src/index.tsx
@@ -1,62 +1,10 @@
-import * as esbuild from 'esbuild-wasm';
-import { useState, useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import App from './App';
 
-const App = () => {
-    const [input, setInput] = useState('');
-    const [code, setCode] = useState('');
-    const [bundler, setBundler] = useState<any>(null);
-
-    const startService = async () => {
-        const esBundler = await esbuild.startService({
-            worker: true,
-            wasmURL: '/esbuild.wasm',
-        });
-        setBundler(esBundler);
-    };
-    useEffect(() => {
-        startService();
-    }, []);
-
-    const onClick = async () => {
-        if (!bundler) {
-            return;
-        }
-
-        // const result = await bundler.transform(input, {
-        //     loader: 'jsx',
-        //     target: 'es2015',
-        // });
-        //
-        // setCode(result.code);
-
-        /**
-         * Use this function and config to intercept ESBuild
-         * to find the path should be for the dependencies
-         */
-        const result = await bundler.build({
-            entryPoints: ['index.ts'],
-            bundle: true,
-            write: false,
-            plugins: [unpkgPathPlugin()],
-        });
-
-        setCode(result.outputFiles[0].text);
-    };
-
-    return (
-        <div>
-            <textarea
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-            />
-            <div>
-                <button onClick={onClick}>Submit</button>
-            </div>
-            <pre>{code}</pre>
-        </div>
-    );
-};
-
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
