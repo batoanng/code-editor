d43ca9d88386b7f773aa93996d986f807c29bcdf
Toan Nguyen <toan.nguyen@hitatek.io>
2022-03-23 22:35:32 +0700

    fix issue import multiple and nested packages

--- packages/jbook/src/plugins/unpkg-path-plugin.ts
+++ packages/jbook/src/plugins/unpkg-path-plugin.ts
@@ -10,12 +10,26 @@ export const unpkgPathPlugin = () => {
                 if (args.path === 'index.js') {
                     return { path: args.path, namespace: 'a' };
                 }
-                if (args.path === 'tiny-test-pkg') {
+
+                /**
+                 * This solves issue import multiple files
+                 */
+                if (args.path.includes('./') || args.path.includes('../')) {
                     return {
-                        path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
-                        namespace: 'a'
+                        namespace: 'a',
+                        path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`).href
                     };
                 }
+                return {
+                    namespace: 'a',
+                    path: `https://unpkg.com/${args.path}`
+                };
+                // if (args.path === 'tiny-test-pkg') {
+                //     return {
+                //         path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+                //         namespace: 'a'
+                //     };
+                // }
             });
 
             build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -25,16 +39,17 @@ export const unpkgPathPlugin = () => {
                     return {
                         loader: 'jsx',
                         contents: `
-                          const message = require('tiny-test-pkg');
+                          const message = require('@joker7t/common-utils');
                           console.log(message);
                         `
                     };
                 }
 
-                const { data } = await axios.get(args.path);
+                const { data, request } = await axios.get(args.path);
                 return {
                     loader: 'jsx',
-                    contents: data
+                    contents: data,
+                    resolveDir: new URL('./', request.responseURL).pathname // This returns exactly file path of nested file instead of path of index file
                 };
             });
         }
