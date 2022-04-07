8f5af21cccc43f32c04baabc8d64c69d9a6a4dce
Toan Nguyen <toan.nguyen@hitatek.io>
2022-04-07 21:28:21 +0700

    set up redux

--- packages/jbook/src/App.tsx
+++ packages/jbook/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import CodeCell from './components/code-cell';
 import TextEditor from './components/text-editor';
+import { Provider } from 'react-redux';
+import { store } from './state';
 
 const App = () => {
     return (
-        <>
+        <Provider store={store}>
             <CodeCell />
             <TextEditor />
-        </>
+        </Provider>
     );
 };
 
